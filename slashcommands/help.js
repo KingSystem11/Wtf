bc68d5fe429f122d9fc009cfb4bc51413fdcfea7
KingSystem11 <162664454+KingSystem11@users.noreply.github.com>
2026-01-23 17:39:23 +0530

    Delete slashcommands directory

--- slashcommands/help.js
+++
@@ -1,8 +0,0 @@
-const helpCommand = require('../commands/help');
-
-module.exports = {
-    data: helpCommand.data,
-    async execute(interaction) {
-        await helpCommand.execute(interaction, [], interaction.client);
-    }
-};
